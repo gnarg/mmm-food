6072adff81a6af186657186b802565657708a08b
Jon Guymon <jon.guymon@gmail.com>
2026-04-09 01:33:20 +0000

    redo init

--- flake.nix
+++ flake.nix
@@ -12,14 +12,15 @@
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs_24
-            claude-code
             git
-            jujutsu
-            dos2unix
+            nodejs_24
           ];
+
+          # Environment variables for Python packages
+          shellHook = ''
+            export EDITOR=/usr/bin/vim
+          '';
         };
       }
     );
 }
-
